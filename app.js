948859e16ec67044e7ff3b79bf8830326d2d6f49
Albrey Brown <albrey.brown@gmail.com>
2017-11-02 12:19:49 -0700

    new stuff

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const
   request = require('request');
 
 var app = express();
-app.set('port', config.port);
+app.set('port', process.env.PORT || 5000);
 app.set('view engine', 'ejs');
 app.use(bodyParser.json({ verify: verifyRequestSignature }));
 app.use(express.static('public'));
@@ -679,9 +679,9 @@ function callSendAPI(messageData) {
 /*
  * Start your server
  */
+
 app.listen(process.env.PORT || app.get('port'), function() {
   console.log('[app.listen] Node app is running on port', app.get('port'));
-  console.log('[app.listen] Node app is running on HEROKU', process.env.PORT);
 });
 
 module.exports = app;
